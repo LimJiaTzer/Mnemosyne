6e63f78b10ed6ac144485945cb51dee06a44f11d
Lim Jia Tzer <limjiatzer@gmail.com>
2025-08-06 09:51:57 +0800

    Initial Commit

---
+++ Mnemosyne/MnemosyneApp.swift
@@ -0,0 +1,17 @@
+//
+//  MnemosyneApp.swift
+//  Mnemosyne
+//
+//  Created by Lim Jia Tzer on 6/8/25.
+//
+
+import SwiftUI
+
+@main
+struct MnemosyneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MnemosyneTests/MnemosyneTests.swift
@@ -0,0 +1,17 @@
+//
+//  MnemosyneTests.swift
+//  MnemosyneTests
+//
+//  Created by Lim Jia Tzer on 6/8/25.
+//
+
+import Testing
+@testable import Mnemosyne
+
+struct MnemosyneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
